1005ac96d53bb1ecdb25b1e5477e98c9f6b3862f
growspiral <68372344+growspiral@users.noreply.github.com>
2020-08-08 20:25:14 +0900

    active_hashの追加、カラムへの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,21 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   def show
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :comment, :price, :category_id, :status_id, :burden_id, :ship_origin_id, :ship_day_id).merge(user_id: current_user.id)
+  end
+
 end
+
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :ship_origin
+  belongs_to_active_hash :ship_day
+  validates :category_id, :status_id, :burden_id, :ship_origin_id, :ship_day_id, numericality: { other_than: 1 }
+  belongs_to :user
 end
