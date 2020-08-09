c161ac4df0fc8ea33da0c834d2841e596f40ba7d
growspiral <68372344+growspiral@users.noreply.github.com>
2020-08-09 13:37:26 +0900

    Active Storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,5 @@ class Item < ApplicationRecord
   belongs_to_active_hash :ship_day
   validates :category_id, :status_id, :burden_id, :ship_origin_id, :ship_day_id, numericality: { other_than: 1 }
   belongs_to :user
+  has_one_attached :image
 end
