caee4cb7945e6fa76526cc976d6cbdf5d7622b68
growspiral <68372344+growspiral@users.noreply.github.com>
2020-08-10 16:45:29 +0900

    販売価格の自動計算機能の実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image
+    name
+    comment
+    category_id
+    status_id
+    burden_id
+    ship_origin_id
+    ship_day_id
+    user_id
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
+describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  @message.image = fixture_file_upload('public/images/test_image.png')
 end
